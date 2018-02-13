0ac6a838f3421228678e31ad1752de12d2f97e14
Thomas Duffy <tomjamesduffy@gmail.com>
2018-02-13 19:47:38 +0000

    Re-factored typos directory

--- typos.rb
+++ typos.rb
@@ -1,18 +1,14 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   while !name.empty?
   # add the student hash to the array
-    students << {name: name.to_sym, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name.to_sym, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
 def print_header
@@ -20,9 +16,9 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  if students.length > 0
-    students.each do |student|
+def print_students_list
+  if @students.length > 0
+    @students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   else
@@ -30,12 +26,41 @@ def print(students)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+interactive_menu
